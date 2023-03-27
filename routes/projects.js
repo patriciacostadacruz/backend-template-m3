99527a1c978350d80e715fd8c41e521119b6eded
patriciacostadacruz <patriciacstcz@gmail.com>
2023-03-27 14:39:35 +0200

    Adds update project route + removes middlewares for faster postman testing

--- routes/projects.js
+++ routes/projects.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res, next) => {
 // @desc    Creates new project
 // @route   POST /projects
 // @access  Private
-router.post("/", isAuthenticated, async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   // add validations so that no empty field for required items is passed
   // also add validations for field types
   try {
@@ -31,11 +31,24 @@ router.post("/", isAuthenticated, async (req, res, next) => {
 // @desc    Get one project
 // @route   GET /projects/:projectId
 // @access  Private
-router.get("/:projectId", isAuthenticated, async (req, res, next) => {
+router.get("/:projectId", async (req, res, next) => {
   const {projectId} = req.params;
   try {
     const project = await Project.findById(projectId);
     res.status(200).json(project);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// @desc    Updates a project
+// @route   PUT /projects/:projectId
+// @access  Private
+router.put("/:projectId", async (req, res, next) => {
+  const {projectId} = req.params;
+  try {
+    const response = await Project.findByIdAndUpdate(projectId, req.body, {new: true});
+    res.status(204).json({message: "Project data updated successfully."});
   } catch (error) {
     console.error(error);
   }
