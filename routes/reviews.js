c5f7fc79fab31d944f86fc7585150d0b624fb4d9
patriciacostadacruz <patriciacstcz@gmail.com>
2023-03-28 12:04:41 +0200

    Adds validations to new review route

--- routes/reviews.js
+++ routes/reviews.js
@@ -6,6 +6,22 @@ const { isAuthenticated, isAdmin } = require("../middlewares/jwt");
 // @route   POST /reviews/new
 // @access  Private
 router.post("/new", async (req, res, next) => {
+  const {
+    title,
+    rating,
+    comment,
+    personRating,
+    personRated
+  } = req.body;
+  if (
+    !title ||
+    !rating ||
+    !comment ||
+    !personRating ||
+    !personRated ) {
+    res.status(400).json({ message: "Please fill all the fields to add a review." });
+    return;
+  }
   try {
     const newReview = await Review.create(req.body);
     res.status(201).json(newReview);
