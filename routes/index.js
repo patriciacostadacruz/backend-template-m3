e2779adf99deade207a0c13d9b6a65f9e55ac72a
patriciacostadacruz <patriciacstcz@gmail.com>
2023-03-29 15:59:12 +0200

    Updates home page route

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,13 @@
 const router = require('express').Router();
 const Project = require("../models/Project");
-const User = require("../models/User");
 
 // @desc    Home page
 // @route   GET /
 // @access  Public
 router.get("/", async (req, res, next) => {
-  const currentUser = req.payload;
-  console.log(currentUser);
   try {
-    const industryProjects = await Project.aggregate([
-      { $match: { industry: currentUser.industry, status: { $ne: "closed" } } },
-      { $sample: { size: 3 } },
-    ]);
-    const randomUsers = await User.aggregate([
-      { $match: { industry: currentUser.industry } },
-      { $sample: { size: 3 } },
-    ]);
-    res.json({ projects: industryProjects, users: randomUsers });
+    const projects = await Project.find({ status: { $ne: "closed" } }).sort({ createdAt: -1 }).limit(3);
+    res.json({ projects });
   } catch (error) {
     console.log(error);
   }
